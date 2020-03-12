0f594d47594d5193a4414cf7c1ec75b2a74126cb
ZSK-CRS <1625127317@qq.com>
2020-03-12 23:39:22 +0800

    Initial commit

---
+++ app/src/main/java/cn/example/testdemo/MainActivity.java
@@ -0,0 +1,41 @@
+package cn.example.testdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RadioGroup;
+
+import cn.example.testdemo.view.MatrixDemoOne;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup radioGroup;
+    private MatrixDemoOne matrix_view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        radioGroup = findViewById(R.id.radioGroup);
+        matrix_view = findViewById(R.id.matrix_view);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.button0){
+                    matrix_view.setTestPoint(0);
+                } else if (checkedId == R.id.button1){
+                    matrix_view.setTestPoint(1);
+                }else if (checkedId == R.id.button2){
+                    matrix_view.setTestPoint(2);
+                }else if (checkedId == R.id.button3){
+                    matrix_view.setTestPoint(3);
+                }else if (checkedId == R.id.button4){
+                    matrix_view.setTestPoint(4);
+                }
+            }
+        });
+    }
+
+}
