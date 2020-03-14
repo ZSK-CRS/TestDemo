324f2fb5d4207562b65d546b09c6c85de0a7e51b
ZSK-CRS <1625127317@qq.com>
2020-03-14 20:12:45 +0800

    initCommit

--- app/src/main/java/cn/example/testdemo/MainActivity.java
+++ app/src/main/java/cn/example/testdemo/MainActivity.java
@@ -2,9 +2,15 @@ package cn.example.testdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.RadioGroup;
 
+import cn.example.testdemo.view.camera.MapView;
 import cn.example.testdemo.view.matrix.MatrixDemoOne;
 
 
@@ -12,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
     private RadioGroup radioGroup;
     private MatrixDemoOne matrix_view;
+    private MapView mapView;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         radioGroup = findViewById(R.id.radioGroup);
         matrix_view = findViewById(R.id.matrix_view);
+        mapView = findViewById(R.id.map_view);
+
+        ObjectAnimator animator1 = ObjectAnimator.ofFloat(mapView, "degreeY", 0, -45);
+        animator1.setDuration(1000);
+        animator1.setStartDelay(500);
+
+        ObjectAnimator animator2 = ObjectAnimator.ofFloat(mapView, "degreeZ", 0, 270);
+        animator2.setDuration(800);
+        animator2.setStartDelay(500);
+
+        ObjectAnimator animator3 = ObjectAnimator.ofFloat(mapView, "fixDegreeY", 0, 30);
+        animator3.setDuration(500);
+        animator3.setStartDelay(500);
+
+        final AnimatorSet set = new AnimatorSet();
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mapView.reset();
+                                set.start();
+                            }
+                        });
+                    }
+                }, 500);
+            }
+        });
+        set.playSequentially(animator1, animator2, animator3);
+        set.start();
 
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
